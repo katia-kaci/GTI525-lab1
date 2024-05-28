03c4aaf4439acd8bdfd2fb524e53510f42d5a327
katia-kaci <114088436+katia-kaci@users.noreply.github.com>
2024-05-28 00:24:02 -0400

    pas important

---
+++ main.js
@@ -0,0 +1,13 @@
+var stations = {};
+
+// Papa.parse('stations.csv', {
+//     complete: function(results) {
+//       console.log('Parsed rows:', results.data);
+//     }
+//   });
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const menu = document.getElementById('menu');
