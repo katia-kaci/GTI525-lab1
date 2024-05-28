7661fed8e0d2c985a76f12c5880f8f64482f8de9
Zaka <zakaria.antar.1@ens.etsmtl.ca>
2024-05-28 09:23:43 -0400

    Mettre les valeurs des stations dans des tableaux

--- main.js
+++ main.js
@@ -7,7 +7,37 @@ var stations = {};
 //   });
 
 
-
+//******************J'AI AJOUTER A REVOIR JE TRAVAIL DESSUS LIVE******************************************** */
 
 // document.addEventListener('DOMContentLoaded', function() {
 //     const menu = document.getElementById('menu');
+
+//import {stations} from '.6720.csv.js'
+// var stations = {};
+// var tabStJohn;
+// const CSVToArray = (data, separateur, skipPremiereLigne) =>{
+//     let valeurs = data
+//     .slice(skipPremiereLigne ? data.indexOf('\n') + 1 : 0)
+//     .split('\n')
+//     .map(v => v.split(separateur))
+
+//     let rows = data.slice(data.indexOf('\n') + 1).split('\n');
+//     let titles = valeurs[0];
+
+//     rows.splice(0,1);
+
+//     return rows.map(row => {
+//     const values = row.split(separateur);
+//     return titles.reduce((object, curr, i) => (object[curr] = values[i], object), {})
+//     });
+//   };
+//   tabStJohn = CSVToArray(stations[6720], ",", true);
+//   console.log(tabStJohn[0]);
+
+function test() {
+    //console.log('test affichage');
+    //document.getElementById("max").innerHTML = tabStJohn[0]['"Extr Min Temp (°C)"'];
+    document.getElementById("annee1").innerHTML = '1992';
+    //document.getElementById("max").innerHTML = tabStJohn[0]['"Extr Min Temp (°C)"'];
+    document.getElementById("annee2").innerHTML = '1990';
+}
