13fd297e2c19b33fe2d55f950a1ece665f768183
Zaka <zakaria.antar.1@ens.etsmtl.ca>
2024-05-30 11:21:55 -0400

    Ajout de quelque trucs je vais nettoyer après

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var stations = {};
+//var stations = {};
 
 // Papa.parse('stations.csv', {
 //     complete: function(results) {
@@ -6,38 +6,76 @@ var stations = {};
 //     }
 //   });
 
+var tabStJohn = csvToArray(stations[6720],",",true);
+var valeursProvince = csvToArray(StationInventoryEN,'","',false);
+var listeProvince = getProvinces();
 
-//******************J'AI AJOUTER A REVOIR JE TRAVAIL DESSUS LIVE******************************************** */
+afficherProvince()
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     const menu = document.getElementById('menu');
-
-//import {stations} from '.6720.csv.js'
-// var stations = {};
-// var tabStJohn;
-// const CSVToArray = (data, separateur, skipPremiereLigne) =>{
-//     let valeurs = data
-//     .slice(skipPremiereLigne ? data.indexOf('\n') + 1 : 0)
-//     .split('\n')
-//     .map(v => v.split(separateur))
-
-//     let rows = data.slice(data.indexOf('\n') + 1).split('\n');
-//     let titles = valeurs[0];
-
-//     rows.splice(0,1);
-
-//     return rows.map(row => {
-//     const values = row.split(separateur);
-//     return titles.reduce((object, curr, i) => (object[curr] = values[i], object), {})
-//     });
-//   };
-//   tabStJohn = CSVToArray(stations[6720], ",", true);
-//   console.log(tabStJohn[0]);
 
 function test() {
-    //console.log('test affichage');
-    //document.getElementById("max").innerHTML = tabStJohn[0]['"Extr Min Temp (°C)"'];
-    document.getElementById("annee1").innerHTML = '1992';
+
+    //document.getElementById("max").innerHTML = tabStJohn[0]['"Extr Min Temp (°C)"'];"
+    document.getElementById("annee1").innerHTML = tabStJohn[0]['"Year"'].replace('"', "").replace('"',"");
     //document.getElementById("max").innerHTML = tabStJohn[0]['"Extr Min Temp (°C)"'];
     document.getElementById("annee2").innerHTML = '1990';
+}
+
+function getProvinces(){
+  let tab=[];
+
+  for(v in valeursProvince){
+    if(valeursProvince[v]['Province']){
+      let prov = valeursProvince[v]['Province'].replace('"',"").replace('"',"").trim();//.replace(" ","")
+      if(!tab.includes(prov))
+        tab.push(prov);
+    }
+  }
+  tab.sort();
+  return tab;
+}
+
+function csvToArray(donnee,separateur,skipLigne1){
+  if(skipLigne1){
+    let valeurs = donnee
+    .slice(donnee.indexOf('\n') + 1 )
+    .split('\n')
+    .map(v => v.split(separateur))
+
+    let rows = donnee.slice(donnee.indexOf('\n') + 1).split('\n');
+    let titles = valeurs[0];
+
+    rows.splice(0,1);
+
+    return rows.map(row => {
+    const values = row.split(separateur);
+    return titles.reduce((obj, actuel, i) => (obj[actuel] = values[i], obj), {})
+    });
+  }
+
+  let valeurs = donnee
+    .slice(donnee.indexOf('\n') + 1)
+    .split('\n')
+    .map(v => v.split(separateur))
+
+    let rows = donnee.slice(donnee.indexOf('\n') + 0).split('\n');
+    let titles = valeurs[3];
+
+    rows.splice(0,5);
+
+    return rows.map(row => {
+    const values = row.split(separateur);
+    return titles.reduce((obj, actuel, i) => (obj[actuel] = values[i], obj), {})
+    });
+}
+
+function afficherProvince() {
+
+  let listeAfficher = ""
+  for(i in listeProvince){
+
+    listeAfficher = listeAfficher+"<ul> <button>"+listeProvince[i]+"</button></ul>";
+  }
+  document.getElementById("listepro").innerHTML = listeAfficher;
+  
 }
