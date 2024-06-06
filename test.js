58eed0e468984efb72a9ccc64b0363dc7abe5237
katia-kaci <114088436+katia-kaci@users.noreply.github.com>
2024-06-06 08:42:38 -0400

    csvToArray

--- test.js
+++ test.js
@@ -1,20 +1,81 @@
-const fs = require("fs");
 const csv = require('csvtojson');
-const { Parser } = require('json2csv');
+var stations = new Map();
+var stationInventory = [];
+let provinces = getProvinces();
 
 (async () => {
     try {
-        const data = await csv().fromFile("scripts/Station Inventory EN.csv");
-        // console.log(data);
+        stationInventory = await csv({
+            headers: ["Name", "Province", "Climate_ID", "Station_ID", "WMO_ID", "TC_ID", "Latitude (Decimal Degrees)", "Longitude (Decimal Degrees)", "Latitude", "Longitude", "Elevation (m)", "First_Year", "Last_Year", "HLY_First_Year", "HLY_Last_Year", "DLY_First_Year", "DLY_Last_Year", "MLY_First_Year", "MLY_Last_Year"]
+        }).fromFile("scripts/Station Inventory EN.csv");
 
-        const dataInCsv = new Parser({ fields: ["Name", "Province", "Climate ID", "Station ID", "WMO ID", "TC ID", "Latitude (Decimal Degrees)", "Longitude (Decimal Degrees)", "Latitude", "Longitude", "Elevation (m)", "First Year", "Last Year", "HLY First Year", "HLY Last Year", "DLY First Year", "DLY Last Year", "MLY First Year", "MLY Last Year"] }).parse(data);
-        fs.writeFileSync("scripts/Station Inventory EN.csv", dataInCsv);
-        console.log(data);
-
+        const ids = ["118", "1865", "2205", "3002", "3328", "3698", "4337", "4789", "4932", "5097", "5251", "5415", "6207", "6358", "6633", "6720"];
+        for (var id of ids) {
+            stations.set(id, await csv().fromFile("scripts/" + id + ".csv"));
+        }
     } catch (err) {
-        console.error("An error occurred:", err);
+        console.error("An error occurred when parsing the scripts: ", err);
     }
 })();
 
-//rouler node test.js dans le terminal pr lire le csv
+function getProvinces() {
+    var provinces = Array.from(new Set(stationInventory.map(station => station.Province)));
+    provinces.shift();
+    provinces.shift();
+    return provinces;
+}
 
+function getStationById(idStation, stationInventory) {
+    return stationInventory.filter(station => station.Station_ID === idStation);
+}
+
+function afficherProvince() {
+    let output = ""
+    for (i in provinces) {
+        output += '<ul> <button value="' + i + '" onclick="afficherNomsStations(this.value)">' + provinces[i] + '</button><ul id="province' + i + '" ></ul></ul>';
+    }
+    document.getElementById("listeprovince").innerHTML = output;
+}
+
+// rouler node test.js dans le terminal pr lire les console.log
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// var listeStations = csvToArray(stations, ",", true);
+// let valeursProvince = csvToArray(StationInventoryEN, '","', false);
+
+let donnee = [];
+let donneeUtiliser = [];
+
+function test2() {
+    document.getElementById("annee1").innerHTML = donneeUtiliser[0]['"Year"'].replace('"', "").replace('"', "");
+    document.getElementById("annee2").innerHTML = donneeUtiliser[1]['"Year"'].replace('"', "").replace('"', "");
+}
+
+function recupererStations(value) {
+    donneeUtiliser = donnee.filter((e) => e['"Station Name"'].replace('"', "").replace('"', "") === value)
+}
+
+function afficherNomsStations(value) {
+    let listeStationsAfficher = [];
+    let val = valeursProvince.filter(e => e["Province"] === provinces[value])
+
+    val.forEach((element) => {
+        let test = listeStations.filter((e) => e['"Climate ID"'].replace('"', "").replace('"', "") == element["Climate ID"]) //element["Climate ID"].replace('"',"").replace('"',""))//=== element["Climate ID"].replace('"',"").replace('"',"").trim())
+        listeStationsAfficher = listeStationsAfficher.concat(test);
+    });
+
+    donnee = listeStationsAfficher;
+
+
+    listeStationsAfficher = [];
+    let baliseAfficher = "";
+    // showProvinces();
+    donnee.map((e) => {
+        if (!listeStationsAfficher.includes(e['"Station Name"'])) {
+            listeStationsAfficher.push(e['"Station Name"']);
+            baliseAfficher = baliseAfficher + '<ul> <button value="' + e['"Station Name"'].replace('"', "").replace('"', "") + '" onclick="recupererStations(this.value)" >' + e['"Station Name"'].replace('"', "").replace('"', "") + '</button></ul>'
+        }
+    })
+    // onclick="afficherNomsStations(this.value)"
+
+    document.getElementById("province" + value).innerHTML = baliseAfficher;
+}
