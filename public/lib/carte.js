4bba9b6a1fe5b718c09932c3595aaded44abd481
katia-kaci <114088436+katia-kaci@users.noreply.github.com>
2024-08-01 04:30:07 -0400

    carte finie

--- public/lib/carte.js
+++ public/lib/carte.js
@@ -1,13 +1,8 @@
 const parser = new DOMParser();
 var map = L.map('map').setView([54, -90], 4);
-let previsionsSelectionnees = false;
-let markers = [], conditionsActuelles = [], previsions = [];
-
-var sunIcon = L.divIcon({
-    className: 'custom-icon',
-    html: '<img src="images/sun.png" style="width: 25px; height: 25px;"/> <div style="font-size: 10px; position: absolute; left: 0; bottom: 1; width: 100%; text-align: center;"></div>',
-    iconSize: [30, 30],
-});
+let previsionsSelectionnees = false, actuellesSelectionnees = true, journeeSelectionnee = false;
+let markers = [], conditionsActuelles = [], previsions = [], journees = [];
+let previsionSelectionnee = "";
 
 async function showTemperature() {
     for (let province in stationJsonMap) {
@@ -17,7 +12,7 @@ async function showTemperature() {
                 if (stations[id]) {
                     let longitude = stations[id].split('\n')[3].split(',')[0].replace(/"/g, '');
                     let latitude = stations[id].split('\n')[3].split(',')[1].replace(/"/g, '');
-                    var marker = L.marker([latitude, longitude], { icon: sunIcon }).addTo(map);
+                    var marker = L.marker([latitude, longitude], { icon: createIcon("", "previsions-icon") }).addTo(map);
                     markers.push(marker);
 
                     const rss_feed = stationJsonMap[province].rss_feed;
@@ -41,7 +36,9 @@ async function showTemperature() {
                                 break;
                             case "Prévisions météo":
                                 let sommaire = entry.getElementsByTagName("title")[0].textContent;
-                                previsionTxt += '<li><b>' + sommaire.split(':')[0] + '</b>' + ': ' + summary + '</li>';
+                                let journee = sommaire.split(':')[0];
+                                journees.push(journee);
+                                previsionTxt += '<li><b>' + journee + '</b>' + ': ' + summary + '</li>';
                                 break;
                         }
                     }
@@ -54,40 +51,100 @@ async function showTemperature() {
             console.error(error.message);
         }
     }
+    chargerBoutons();
     updatePopup();
 }
 
+function chargerBoutons() {
+    const container = document.getElementById('radio-container')
+    journees = Array.from(new Set(journees));
+    for (let journee of journees) {
+        const radio = document.createElement('input');
+        radio.type = 'radio';
+        radio.id = journee;
+        radio.name = 'options';
+        radio.value = journee;
+
+        const label = document.createElement('label');
+        label.htmlFor = journee;
+        label.textContent = journee;
+
+        container.appendChild(radio);
+        container.appendChild(label);
+        container.appendChild(document.createElement('br'))
+    }
+
+    const radios = document.querySelectorAll('input[type="radio"][name="options"]');
+    radios.forEach(radio => {
+        radio.addEventListener('change', () => {
+            if (radio.checked) {
+                previsionsSelectionnees = radio.value == "option previsions" ? true : false;
+                actuellesSelectionnees = radio.value == "option actuelles" ? true : false;
+                if (radio.value != "option previsions" && radio.value != "option actuelles") {
+                    journeeSelectionnee = true;
+                    previsionsSelectionnees = false;
+                    actuellesSelectionnees = false;
+                    previsionSelectionnee = radio.value;
+                }
+                else journeeSelectionnee = false;
+            }
+            updatePopup();
+        });
+    });
+}
+
 function updatePopup() {
-    console.log("update")
+    // console.log("update")
     for (let i = 0; i < markers.length; i++) {
         if (previsionsSelectionnees) {
+            markers[i].setIcon(createIcon("", "previsions-icon"));
             markers[i].bindPopup(`<div class="scrollable-popup">${previsions[i]}</div>`);
         }
-        else {
+        else if (actuellesSelectionnees) {
             let conditionsString = conditionsActuelles[i];
             const start = conditionsString.indexOf('<b>Température:</b>');
             const end = conditionsString.indexOf('&deg');
-            const temperature = conditionsString.substring(start, end).replace('<b>Température:</b>', '').trim();
-            let updatedIcon = conditionsString.includes('nuage') ? createCloudIcon(temperature) : createSunIcon(temperature);
+            const temperature = conditionsString.substring(start, end).replace('<b>Température:</b>', '').trim() + "°C";
+            let updatedIcon;
+            if (conditionsString.includes('averses')) updatedIcon = createIcon(temperature, "pluie");
+            else updatedIcon = conditionsString.includes('nuage') || conditionsString.includes('Nuage') || conditionsString.includes('brouillard') || conditionsString.includes('Brouillard') ? createIcon(temperature, "cloud") : createIcon(temperature, "sun");
             markers[i].setIcon(updatedIcon);
             markers[i].bindPopup(conditionsString);
+        }
+        else if (journeeSelectionnee) {
+            let lignes = previsions[i].split("<li>");
+            console.log(lignes)
+            for (let ligne of lignes) {
+                if (ligne.includes(previsionSelectionnee + "</b>")) {
+                    const start = ligne.includes('Maximum') ? ligne.indexOf('Maximum') : ligne.indexOf('Minimum');
+                    let temperature = "";
+                    temperature = ligne.substring(start + "Maximum".length).trim().split(' ')[0].replace(".", "") + "°C";
 
+                    let updatedIcon;
+                    if (ligne.includes('nuit')) {
+                        if (ligne.includes('averses')) updatedIcon = createIcon(temperature, "nuit-nuage-pluie");
+                        else updatedIcon = ligne.includes('nuage') || ligne.includes('Nuage') ? createIcon(temperature, "nuit-nuage") : createIcon(temperature, "nuit-degage");
+                    }
+                    else {
+                        if (ligne.includes('averses')) updatedIcon = createIcon(temperature, "pluie");
+                        else updatedIcon = ligne.includes('nuage') || ligne.includes('Nuage') ? createIcon(temperature, "cloud") : createIcon(temperature, "sun");
+                    }
+                    markers[i].setIcon(updatedIcon);
+                    markers[i].bindPopup(ligne.replace('</li>', ''));
+                }
+                if (!previsions[i].includes(previsionSelectionnee + "</b>")) {
+                    markers[i].setIcon(createIcon("", "prevision-inconnue"));
+                    markers[i].bindPopup("Pas encore de prévisions pour cette journée.");
+                }
+            }
         }
     }
 }
 
-function createCloudIcon(temperature) {
+function createIcon(temperature, imageNom) {
     return L.divIcon({
         className: 'custom-icon',
-        html: `<img src="images/cloud.png" style="width: 25px; height: 25px;"/> <div style="font-size: 10px; position: absolute; left: 0; bottom: 1; width: 100%; text-align: center;">${temperature}°C</div>`,
-        iconSize: [30, 30]
-    });
-}
-
-function createSunIcon(temperature) {
-    return L.divIcon({
-        className: 'custom-icon',
-        html: `<img src="images/sun.png" style="width: 25px; height: 25px;"/> <div style="font-size: 10px; position: absolute; left: 0; bottom: 1; width: 100%; text-align: center;">${temperature}°C</div>`,
+        html: `<img src="images/${imageNom}.png" style="width: 25px; height: 25px;"/> <div style="font-size: 10px; position: absolute; left: 0; bottom: 1; width: 100%; text-align: center;">${temperature}</div>`,
         iconSize: [30, 30]
     });
 }
@@ -98,16 +155,6 @@ document.addEventListener('DOMContentLoaded', async function () {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-
-    const radios = document.querySelectorAll('input[type="radio"][name="options"]');
-    radios.forEach(radio => {
-        radio.addEventListener('change', () => {
-            if (radio.checked) {
-                previsionsSelectionnees = radio.value == "option previsions" ? true : false;
-            }
-            updatePopup();
-        });
-    });
 
     try {
         const response = await fetch('/station_mapping');
